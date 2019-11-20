bf433ba09972334602df9f51e48877ef319ce8f2
melayer-01 <melayer.rajan@gmail.com>
2019-11-20 10:28:19 +0530

    initial commit.

--- src/FirstDemo.java
+++ src/FirstDemo.java
@@ -1,2 +1,22 @@
 public class FirstDemo {
+
+    int b=20;
+
+    int addition(float a){
+      //  int a=10;
+    //    int c=a+b;
+       // System.out.println("Addition : "+(a+b));
+        return (int) (a+b);
+    }
+
+
+
+    void sub(){
+        b=20;
+    }
+
+    public static void main(String[] args) {
+        FirstDemo demo=new FirstDemo();
+        System.out.println("addition : "+demo.addition((float) 10.5));
+    }
 }
