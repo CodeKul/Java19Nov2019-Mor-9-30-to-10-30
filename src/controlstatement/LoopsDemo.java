af57981452a7a43fbea006579bc0c7bb8715d75f
melayer-01 <melayer.rajan@gmail.com>
2019-12-03 10:38:05 +0530

    loops completed.

--- src/controlstatement/LoopsDemo.java
+++ src/controlstatement/LoopsDemo.java
@@ -1,4 +1,36 @@
 package controlstatement;
 
 public class LoopsDemo {
+
+    private void forloopDemo(){
+
+        for (int i=0;i<3;i++){
+            System.out.println(i);
+        }
+
+    }
+
+    private void whileLoopDemo(){
+        int i=0;
+        while (i<3){
+            System.out.println(i);
+            i++;
+        }
+
+    }
+
+    private void doWhileDemo(){
+        int i=3;
+        do {
+            System.out.println(i);
+            i++;
+        }while (i<3);
+        System.out.println("after do while : "+i);
+    }
+
+    public static void main(String[] args) {
+        LoopsDemo demo=new LoopsDemo();
+        demo.doWhileDemo();
+    }
+
 }
