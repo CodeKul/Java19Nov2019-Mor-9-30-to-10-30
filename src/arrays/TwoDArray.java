1d577b7339accd5bdcfa0e46d0d9b2626b8babfe
melayer-01 <melayer.rajan@gmail.com>
2019-12-16 10:28:04 +0530

    constructor.

--- src/arrays/TwoDArray.java
+++ src/arrays/TwoDArray.java
@@ -1,16 +1,26 @@
 package arrays;
 
+import java.util.Scanner;
+
 public class TwoDArray {
 
 
     private void showArray(){
 
-        int array[][]={{1,2,3},{4,5,6},{7,8,9}};
-
+       // int array[][]={{1,2,3},{4,5,6},{7,8,9}};
+        int array[][]=new int[3][3];
+        Scanner scanner=new Scanner(System.in);
         for (int i=0;i<array.length;i++){
             for (int j=0;j<array.length;j++){
-                System.out.print(array[i][j]+" ");
+                System.out.println("Enter array of "+i+""+j+" : ");
+                array[i][j]=scanner.nextInt();
             }
+        }
+        for (int i=0;i<array.length;i++){
+            for (int j=array.length-1;j>=0;j--){
+                System.out.print(array[j][i]+" ");
+            }
+
             System.out.println();
         }
 
@@ -35,4 +45,14 @@ public class TwoDArray {
 3 2 1
 6 5 4
 9 8 7
+
+1 2 3
+4 5 6
+7 8 9
+*/
+
+/*
+00 01 02
+10 11 12
+20 21 22
 */
---
+++ src/constructor/ConstructorDemo.java
@@ -0,0 +1,24 @@
+package constructor;
+
+public class ConstructorDemo {
+
+    ConstructorDemo(int a){
+
+
+    }
+
+/*
+    ConstructorDemo(){
+
+    }
+*/
+
+    ConstructorDemo(int a,int b){
+
+    }
+
+    public static void main(String[] args) {
+        ConstructorDemo demo=new ConstructorDemo(1);
+    }
+
+}
